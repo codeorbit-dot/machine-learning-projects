3ae44457ee87ea3686617f499dd70c1123d7461e
codeorbit-dot <tarun.benakanakonda@aifactory.technology>
2026-07-22 11:45:42 +0530

    update

--- Day-3/p3.py
+++ Day-3/p3.py
@@ -1,11 +1,40 @@
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+from math import pi
 
-    def display(self):
-        print("Name:", self.name)
-        print("Marks:", self.marks)
+class Shape:
+    def area(self):
+        pass
 
-s1 = Student("Tarun", 95)
-s1.display()
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+
+    def area(self):
+        return pi * self.r ** 2
+
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+
+n = int(input("Enter shape (1.circle/2.rectangle): "))
+
+if n == 1:
+    r = float(input("Enter radius: "))
+    shape = Circle(r)
+
+elif n == 2:
+    l = float(input("Enter length: "))
+    w = float(input("Enter width: "))
+    shape = Rectangle(l, w)
+
+else:
+    print("Invalid Shape")
+    exit()
+
+print(f"Area = {shape.area():.2f}")
